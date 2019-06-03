7f4323d1351f21856b4702f9e36450f460a2c756
Jack Mahoney <jm888@uw.edu>
2019-06-03 14:14:10 -0700

    fixed pylint errors

--- awsforyou/total_time_component.py
+++ awsforyou/total_time_component.py
@@ -6,21 +6,21 @@ from scipy.optimize import curve_fit
 warnings.filterwarnings('ignore')
 
 
-def func_log(x, a, b):
+def func_log(data, a_factor, y_int):
     """A logarithmic function with y-intercept equal to zero"""
-    y = a * np.log(x) + b
+    y = a_factor * np.log(data) + y_int
     return y
 
 
-def func_sqrd(x, a, b):
+def func_sqrd(data, a_factor, y_int):
     """A squaring function with y-intercept equal to zero"""
-    y = a*np.power(x, 2)+b
+    y = a_factor * np.power(data, 2) + y_int
     return y
 
 
-def func_linear(x, a, b):
+def func_linear(data, a_factor, y_int):
     """A linear function with y-intercept equal to zero"""
-    y = a * x + b
+    y = a_factor * data + y_int
     return y
 
 
@@ -28,18 +28,18 @@ def find_total_time(times, row_percents=(1, 5, 10)):
     """Given a list of three times and the percentages of a data set used to \
     calculate those times, this function will estimate the time required to \
     run the entire data set."""
-    popt_linear, pcov_linear = curve_fit(func_linear,  row_percents, times)
+    popt_linear = curve_fit(func_linear,  row_percents, times)[0]
     a_linear = popt_linear[0].flatten()
     b_linear = popt_linear[1].flatten()
     resid_linear = np.linalg.norm(times-func_linear(row_percents,
                                                     a_linear, b_linear))
 
-    popt_sqrd, pcov_sqrd = curve_fit(func_sqrd,  row_percents,  times)
+    popt_sqrd = curve_fit(func_sqrd,  row_percents,  times)[0]
     a_sqrd = popt_sqrd[0].flatten()
     b_sqrd = popt_sqrd[1].flatten()
     resid_sqrd = np.linalg.norm(times-func_sqrd(row_percents, a_sqrd, b_sqrd))
 
-    popt_log, pcov_log = curve_fit(func_log,  row_percents,  times)
+    popt_log = curve_fit(func_log,  row_percents,  times)[0]
     a_log = popt_log[0].flatten()
     b_log = popt_log[1].flatten()
     resid_log = np.linalg.norm(times-func_log(row_percents, a_log, b_log))
