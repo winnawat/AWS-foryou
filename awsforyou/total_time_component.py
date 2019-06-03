a15b731d6b5a752e738b30620b1189dd9fead2af
Jack Mahoney <jm888@uw.edu>
2019-06-03 14:23:27 -0700

    fixed pylint errors

--- awsforyou/total_time_component.py
+++ awsforyou/total_time_component.py
@@ -8,38 +8,35 @@ warnings.filterwarnings('ignore')
 
 def func_log(data, a_factor, y_int):
     """A logarithmic function with y-intercept equal to zero"""
-    y = a_factor * np.log(data) + y_int
-    return y
+    return = a_factor * np.log(data) + y_int
 
 
 def func_sqrd(data, a_factor, y_int):
     """A squaring function with y-intercept equal to zero"""
-    y = a_factor * np.power(data, 2) + y_int
-    return y
+    return a_factor * np.power(data, 2) + y_int
 
 
 def func_linear(data, a_factor, y_int):
     """A linear function with y-intercept equal to zero"""
-    y = a_factor * data + y_int
-    return y
+    return a_factor * data + y_int
 
 
 def find_total_time(times, row_percents=(1, 5, 10)):
     """Given a list of three times and the percentages of a data set used to \
     calculate those times, this function will estimate the time required to \
     run the entire data set."""
-    popt_linear = curve_fit(func_linear,  row_percents, times)[0]
+    popt_linear = curve_fit(func_linear, row_percents, times)[0]
     a_linear = popt_linear[0].flatten()
     b_linear = popt_linear[1].flatten()
     resid_linear = np.linalg.norm(times-func_linear(row_percents,
                                                     a_linear, b_linear))
 
-    popt_sqrd = curve_fit(func_sqrd,  row_percents,  times)[0]
+    popt_sqrd = curve_fit(func_sqrd, row_percents, times)[0]
     a_sqrd = popt_sqrd[0].flatten()
     b_sqrd = popt_sqrd[1].flatten()
     resid_sqrd = np.linalg.norm(times-func_sqrd(row_percents, a_sqrd, b_sqrd))
 
-    popt_log = curve_fit(func_log,  row_percents,  times)[0]
+    popt_log = curve_fit(func_log, row_percents, times)[0]
     a_log = popt_log[0].flatten()
     b_log = popt_log[1].flatten()
     resid_log = np.linalg.norm(times-func_log(row_percents, a_log, b_log))
