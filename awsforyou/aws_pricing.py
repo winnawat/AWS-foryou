76733a817cbb36beff6a807a6caa0679d10d39a6
Vivek Kumar <vivekuma@uw.edu>
2019-06-10 15:33:20 -0700

    update to get price of multiple instances at once

--- awsforyou/aws_pricing.py
+++ awsforyou/aws_pricing.py
@@ -1,3 +1,5 @@
+import logging
+
 import boto3
 
 import pandas as pd
@@ -6,6 +8,14 @@ ON_DEMAND_PRICING_CSV = 'https://tinyurl.com/yykw7p8c'
 ON_DEMAND_PRICES = pd.read_csv(ON_DEMAND_PRICING_CSV,
                                error_bad_lines=False,
                                warn_bad_lines=False)
+
+DF_COL_INSTANCE_TYPE = 'instance_type'
+DF_COL_REGION = 'region'
+DF_COL_SPOT_PRICE = 'spot_price'
+DF_COL_ON_DEMAND_PRICE = 'on_demand_price'
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def get_all_regions():
@@ -51,27 +61,40 @@ def get_on_demand_price(instance_type, region_name):
     return price.item()
 
 
-def get_instance_pricing(instance_type):
+def get_instance_pricing(instance_types):
     """
     Get the spot and on demand price of an instance type
     in all the regions at current instant
-    :param instance_type: EC2 instance type
+    :param instance_types: EC2 instance type
     :return: a pandas DataFrame with columns as
              region, spot price and on demand price
     """
     all_regions = get_all_regions()
 
+    instance_type_list = []
+    regions_list = []
     spot_price_list = []
     on_demand_price_list = []
 
-    for region_name in all_regions:
-        spot_price = get_spot_price(instance_type, region_name)
-        on_demand_price = get_on_demand_price(instance_type,
-                                              region_name)
+    for instance_type in instance_types:
+        for region_name in all_regions:
+            try:
+                spot_price = get_spot_price(instance_type, region_name)
+                on_demand_price = get_on_demand_price(instance_type,
+                                                      region_name)
+                instance_type_list.append(instance_type)
+                regions_list.append(region_name)
+                spot_price_list.append(spot_price)
+                on_demand_price_list.append(on_demand_price)
+            except (IndexError, KeyError):
+                LOGGER.warning('Failed to fetch Price for instance {} in '
+                               'region {}'.format(instance_type, region_name))
 
-        spot_price_list.append(spot_price)
-        on_demand_price_list.append(on_demand_price)
+        print(len(instance_type_list))
+        print(len(all_regions))
+        print(len(instance_type_list))
 
-    return pd.DataFrame({'region': all_regions,
-                         'spot_price': spot_price_list,
-                         'on_demand_price': on_demand_price_list})
+    return pd.DataFrame({DF_COL_INSTANCE_TYPE: instance_type_list,
+                         DF_COL_REGION: regions_list,
+                         DF_COL_SPOT_PRICE: spot_price_list,
+                         DF_COL_ON_DEMAND_PRICE: on_demand_price_list})
