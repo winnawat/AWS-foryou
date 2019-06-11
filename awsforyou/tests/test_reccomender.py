51db72517426d0d0b3b10d4b819375498b645151
Jack Mahoney <jm888@uw.edu>
2019-06-11 12:07:21 -0700

    added tests for time and price

--- awsforyou/tests/test_reccomender.py
+++ awsforyou/tests/test_reccomender.py
@@ -24,10 +24,11 @@ class TestRecommender(unittest.TestCase):
         test to see if added times dataframe is empty
         """
         benchmark_df = rc.get_benchmark_data()
-        times, percents = [2,4,6], [1,5,10]
+        times, percents = [2, 4, 6], [1, 5, 10]
         est_time_user = tt.find_total_time(times, percents)
         user_benchmark = br.run_benchmark()
-        est_time_aws = benchmark_df[['runtime']] / user_benchmark * est_time_user[0]
+        est_time_aws = benchmark_df[['runtime']] / \
+            user_benchmark * est_time_user[0]
         benchmark_df["estimated_time_aws"] = est_time_aws
         self.assertGreater(benchmark_df.shape[0], 0)
 
@@ -36,28 +37,33 @@ class TestRecommender(unittest.TestCase):
         test to see if added times dataframe is empty
         """
         benchmark_df = rc.get_benchmark_data()
-        times, percents = [2,4,6], [1,5,10]
+        times, percents = [2, 4, 6], [1, 5, 10]
         est_time_user = tt.find_total_time(times, percents)
         user_benchmark = br.run_benchmark()
-        est_time_aws = benchmark_df[['runtime']] / user_benchmark * est_time_user[0]
+        est_time_aws = benchmark_df[['runtime']] \
+            / user_benchmark * est_time_user[0]
         benchmark_df["estimated_time_aws"] = est_time_aws
         self.assertGreater(benchmark_df.shape[0], 0)
 
     def test_add_estimated_price(self):
         """
-        This function tests adding the spot and on-demand pricing to the dataframe
+        This function tests adding the spot and on-demand pricing
+        to the dataframe
         """
         benchmark_df = rc.get_benchmark_data()
         times, percents = [2, 4, 6], [1, 5, 10]
         est_time_user = tt.find_total_time(times, percents)
         user_benchmark = br.run_benchmark()
-        est_time_aws = benchmark_df[['runtime']] / user_benchmark * est_time_user[0]
+        est_time_aws = benchmark_df[['runtime']] \
+            / user_benchmark * est_time_user[0]
         benchmark_df["estimated_time_aws"] = est_time_aws
         instance_types = benchmark_df["instance_type"].tolist()
         price = ap.get_instance_pricing(instance_types)
         complete_df = pd.merge(benchmark_df, price, on="instance_type")
         complete_df["est_cost_spot_price"] = \
-            complete_df["estimated_time_aws"] * complete_df["spot_price"] / 3600
+            complete_df["estimated_time_aws"] \
+            * complete_df["spot_price"] / 3600
         complete_df["est_cost_on_demand_price"] = \
-            complete_df["estimated_time_aws"] * complete_df["on_demand_price"] / 3600
+            complete_df["estimated_time_aws"] \
+            * complete_df["on_demand_price"] / 3600
         self.assertGreater(complete_df.shape[0], 0)
