796348cc9db0761b308f3bf3c88b14684c02f60c
Jack Mahoney <jm888@uw.edu>
2019-06-11 09:41:22 -0700

    created test for recommender module

---
+++ awsforyou/tests/test_reccomender.py
@@ -0,0 +1,16 @@
+"""this module contains three tests for the total time component"""
+import unittest
+from awsforyou import recommender as rc
+
+
+class TestRecommender(unittest.TestCase):
+    """
+    this class contains tests
+    """
+
+    def test_for_benchmark_df_empty(self):
+        """
+        test to see if benchmark dataframe is empty
+        """
+        benchmark_df = rc.get_benchmark_data()
+        self.assertGreater(benchmark_df.shape[0], 0)
