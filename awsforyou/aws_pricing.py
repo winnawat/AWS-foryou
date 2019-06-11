5f72e83b2388a9e647147cc5dc8abc8ba96b35eb
Vivek Kumar <vivekuma@uw.edu>
2019-06-11 14:09:23 -0700

    optimize aws price fetching

--- awsforyou/aws_pricing.py
+++ awsforyou/aws_pricing.py
@@ -28,37 +28,55 @@ def get_all_regions():
     return [region['RegionName'] for region in response['Regions']]
 
 
-def get_spot_price(instance_type, region_name):
+def get_spot_price(instance_types, region_name):
     """
     Get spot price at current instant of an instance in a
     given region
-    :param instance_type: EC2 instance type
+    :param instance_types: EC2 instance type
     :param region_name: AWS regions name
-    :return: a floating point number - spot price
+    :return: a dictionary with instance type as key and  spot price as value
     """
     ec2_client = boto3.client('ec2', region_name=region_name)
     response = ec2_client.describe_spot_price_history(
-        InstanceTypes=[instance_type],
+        InstanceTypes=instance_types,
         ProductDescriptions=['Linux/UNIX'],
-        MaxResults=1)
+        MaxResults=len(instance_types) * 6)
 
-    spot_price = response['SpotPriceHistory'][0]['SpotPrice']
-    return float(spot_price)
+    spot_prices = {}
+
+    spot_price_list = response['SpotPriceHistory']
+    for sp in spot_price_list:
+        if sp['InstanceType'] not in spot_prices:
+            spot_prices[sp['InstanceType']] = float(sp['SpotPrice'])
+
+    instance_type_list = []
+    spot_price_list = []
+    for instance_type, spot_price in spot_prices.items():
+        instance_type_list.append(instance_type)
+        spot_price_list.append(spot_price)
+
+    return pd.DataFrame({DF_COL_INSTANCE_TYPE: instance_type_list,
+                         DF_COL_SPOT_PRICE: spot_price_list})
 
 
-def get_on_demand_price(instance_type, region_name):
+def get_on_demand_price(instance_types, region_name):
     """
     Get on demand price at current instant of an instance in a
     given region
-    :param instance_type: EC2 instance type
+    :param instance_types: EC2 instance types
     :param region_name: AWS regions name
-    :return: a floating point number - on demand price
+    :return: a dictionary as instance type as key and
     """
-    price = ON_DEMAND_PRICES.loc[
-        (ON_DEMAND_PRICES['instanceType'] == instance_type) &
+    instance_prices = ON_DEMAND_PRICES.loc[
+        (ON_DEMAND_PRICES['instanceType'].isin(instance_types)) &
         (ON_DEMAND_PRICES['region'] == region_name) &
-        (ON_DEMAND_PRICES['operatingSystem'] == 'Linux')]['price']
-    return price.item()
+        (ON_DEMAND_PRICES['operatingSystem'] == 'Linux')]
+
+    instance_type_list = instance_prices['instanceType']
+    on_demand_price_list = instance_prices['price']
+
+    return pd.DataFrame({DF_COL_INSTANCE_TYPE: instance_type_list,
+                         DF_COL_ON_DEMAND_PRICE: on_demand_price_list})
 
 
 def get_instance_pricing(instance_types):
@@ -71,26 +89,28 @@ def get_instance_pricing(instance_types):
     """
     all_regions = get_all_regions()
 
-    instance_type_list = []
-    regions_list = []
-    spot_price_list = []
-    on_demand_price_list = []
+    price_df = pd.DataFrame({DF_COL_INSTANCE_TYPE: [],
+                             DF_COL_REGION: [],
+                             DF_COL_SPOT_PRICE: [],
+                             DF_COL_ON_DEMAND_PRICE: []})
 
-    for instance_type in instance_types:
-        for region_name in all_regions:
-            try:
-                spot_price = get_spot_price(instance_type, region_name)
-                on_demand_price = get_on_demand_price(instance_type,
-                                                      region_name)
-                instance_type_list.append(instance_type)
-                regions_list.append(region_name)
-                spot_price_list.append(spot_price)
-                on_demand_price_list.append(on_demand_price)
-            except (IndexError, KeyError):
-                LOGGER.warning('Failed to fetch Price for instance {} in '
-                               'region {}'.format(instance_type, region_name))
+    for region_name in all_regions:
+        try:
+            spot_prices = get_spot_price(instance_types, region_name)
+            on_demand_prices = get_on_demand_price(instance_types, region_name)
+            both_prices = pd.merge(spot_prices, on_demand_prices,
+                                   on=DF_COL_INSTANCE_TYPE)
 
-    return pd.DataFrame({DF_COL_INSTANCE_TYPE: instance_type_list,
-                         DF_COL_REGION: regions_list,
-                         DF_COL_SPOT_PRICE: spot_price_list,
-                         DF_COL_ON_DEMAND_PRICE: on_demand_price_list})
+            n_rows = both_prices.shape[0]
+            region_list = n_rows * [region_name]
+            both_prices[DF_COL_REGION] = region_list
+            both_prices = both_prices[[DF_COL_INSTANCE_TYPE, DF_COL_REGION,
+                                       DF_COL_SPOT_PRICE,
+                                       DF_COL_ON_DEMAND_PRICE]]
+
+            price_df = price_df.append(both_prices)
+        except (IndexError, KeyError):
+            LOGGER.warning('Failed to fetch Price for region {} in'
+                           .format(region_name))
+
+    return price_df
--- awsforyou/tests/test_aws_pricing.py
+++ awsforyou/tests/test_aws_pricing.py
@@ -1,6 +1,7 @@
 import unittest
 
 import pandas as pd
+import numpy as np
 
 from awsforyou import aws_pricing
 
@@ -13,34 +14,43 @@ class TestAwsPricing(unittest.TestCase):
         self.assertGreater(len(regions), 0)
 
     def test_get_spot_pricing(self):
-        price = aws_pricing.get_spot_price('t3.large', 'us-east-1')
+        price = aws_pricing.get_spot_price(['t3.large',
+                                            'c5.18xlarge'], 'us-east-1')
+        self.assertIsInstance(price, pd.DataFrame)
         self.assertIsNotNone(price)
 
     def test_get_on_demand_pricing(self):
-        price = aws_pricing.get_on_demand_price('t3.large', 'us-east-1')
-        self.assertEquals(price, 0.0832)
+        price = aws_pricing.get_on_demand_price(['t3.large',
+                                                 'c5.18xlarge'], 'us-east-1')
+        t3_price_exp = 0.0832
+        c5_price_exp = 3.0600
+        t3_price = price[price['instance_type'] ==
+                         't3.large']['on_demand_price'].item()
+        c5_price = price[price['instance_type'] ==
+                         'c5.18xlarge']['on_demand_price'].item()
 
-    def test_get_all_pricing_one_instance(self):
-        price_df = aws_pricing.get_instance_pricing(['t3.large'])
-        self.assertIsInstance(price_df, pd.DataFrame)
-        columns = list(price_df.columns)
-        expected_columns = ['instance_type', 'region',
-                            'spot_price', 'on_demand_price']
-        self.assertEquals(columns, expected_columns)
+        self.assertEquals(t3_price, t3_price_exp)
+        self.assertEquals(c5_price, c5_price_exp)
 
     def test_get_all_pricing_multiple_instance(self):
         instance_type_list = ['c5.18xlarge', 'm5a.large', 'c5.xlarge']
         price_df = aws_pricing.get_instance_pricing(instance_type_list)
         self.assertIsInstance(price_df, pd.DataFrame)
         columns = list(price_df.columns)
-        expected_columns = ['instance_type', 'region',
-                            'spot_price', 'on_demand_price']
+        expected_columns = [aws_pricing.DF_COL_INSTANCE_TYPE,
+                            aws_pricing.DF_COL_REGION,
+                            aws_pricing.DF_COL_SPOT_PRICE,
+                            aws_pricing.DF_COL_ON_DEMAND_PRICE]
+
         instance_types_df = price_df[aws_pricing.DF_COL_INSTANCE_TYPE].unique()
         self.assertEquals(columns, expected_columns)
         self.assertCountEqual(instance_type_list, instance_types_df)
 
     def test_spot_less_than_on_demand(self):
-        spot_price = aws_pricing.get_spot_price('t3.large', 'us-east-1')
-        on_demand_price = aws_pricing.get_on_demand_price('t3.large',
-                                                          'us-east-1')
-        self.assertLessEqual(spot_price, on_demand_price)
+        instance_type_list = ['c5.18xlarge', 'm5a.large', 'c5.xlarge']
+        price_df = aws_pricing.get_instance_pricing(instance_type_list)
+        spot_prices = price_df[aws_pricing.DF_COL_SPOT_PRICE]
+        on_demand_prices = price_df[aws_pricing.DF_COL_ON_DEMAND_PRICE]
+        compare_results = spot_prices <= on_demand_prices
+
+        self.assertTrue(np.all(compare_results))
